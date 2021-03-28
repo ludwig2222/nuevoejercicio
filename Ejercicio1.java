5ee2b6119690de3f663e60ae94a48e0f0e5e6f9c
Luis Miguel Bernabé <lbernabe@educamadrid.org>
2021-03-28 06:55:54 +0200

    añadida tercera versión

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
-
+/**
+* @author Luis Miguel Bernabé
+* @version 3.0
+*/
 public class Ejercicio1 {
-
+    /**
+	* @param num Valor entero para el que se realiza la suma de sus digitos
+	* @return devuelve un entero
+	*/
 	public static int sumaDig(int num) {
 	
 	   if (num<=9)
@@ -15,9 +21,11 @@ public class Ejercicio1 {
 
 		Scanner sc=new Scanner(System.in);
 		
+		System.out.println("Introduzca un nº positivo:");
+		
 		int num=sc.nextInt();
 		
-		System.out.println(sumaDig(num));
+		System.out.println("Con recursividad: "+sumaDig(num));
 		
 		
    }
